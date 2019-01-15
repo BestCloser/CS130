09764b0afccb772db5cf58a4a19720bb1e38b34c
Daniel <dli015@ucr.edu>
2019-01-15 10:43:24 -0800

    Update render_world.cpp

--- proj-rt/proj-rt-files/render_world.cpp
+++ proj-rt/proj-rt-files/render_world.cpp
@@ -29,8 +29,15 @@ Hit Render_World::Closest_Intersection(const Ray& ray)
 // set up the initial view ray and call
 void Render_World::Render_Pixel(const ivec2& pixel_index)
 {
-    TODO; // set up the initial view ray here
-    Ray ray;
+    //TODO; // set up the initial view ray here
+    vec3 end_point = camera.position;
+	vec3 direction = (end_point - camera.World_Position(pixel_index)).normalized();
+	
+	Ray ray;
+	
+	ray.endpoint = end_point;
+	ray.direction = direction;
+	
     vec3 color=Cast_Ray(ray,1);
     camera.Set_Pixel(pixel_index,Pixel_Color(color));
 }
