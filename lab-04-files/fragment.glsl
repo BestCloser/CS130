73bcf6e49826ad038998a01a67ca07b7be18c8ef
Daniel <dli015@ucr.edu>
2019-02-11 23:05:30 -0800

    Updated fragment.glsl

--- lab-04-files/fragment.glsl
+++ lab-04-files/fragment.glsl
@@ -4,8 +4,26 @@ varying vec3 light_direction;
 
 void main()
 {
-    vec4 ambient = vec4(1, 0, 0, 1);
-    vec4 diffuse = vec4(0, 1, 0, 1);
-    vec4 specular = vec4(0, 0, 1, 1);
-    gl_FragColor = ambient + diffuse + specular;
+    vec4 normal4 = vec4(normal[0], normal[1], normal[2], 0);
+
+    vec4 finalColor = gl_LightModel.ambient;
+
+    for (int i = 0; i < gl_MaxLights; i++)
+    {
+        vec4 l_ray = vec4(light_direction[0], light_direction[1], light_direction[2], 1);
+        // replacing last component from 0 to 1 fixes specular component displaying Specular Monkey
+        vec4 e_ray = normalize(-position);
+        vec4 r_ray = normalize(2.0 * dot(l_ray, normal4) * normal4 - l_ray);
+
+        vec4 ambient = gl_LightSource[i].ambient * gl_FrontMaterial.ambient;
+        // not including gl_LightModel.ambient because green becomes darker and doesn't match the test
+
+        vec4 diffuse = gl_LightSource[i].diffuse * gl_FrontMaterial.diffuse * max(dot(normalize(l_ray), normal4), 0.0);
+
+	    vec4 specular = gl_LightSource[i].specular * gl_FrontMaterial.specular * pow(max(dot(r_ray, e_ray), 0.0), gl_FrontMaterial.shininess);
+
+	    finalColor += ambient + diffuse + specular;
+    }
+
+    gl_FragColor = finalColor;
 }
