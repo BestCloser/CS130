56890241d71094bf280db73fdea71f936aeddebf
Daniel <dli015@ucr.edu>
2019-03-04 17:44:39 -0800

    LOL
    fat 0

--- proj-gl/proj-gl-files/driver_state.cpp
+++ proj-gl/proj-gl-files/driver_state.cpp
@@ -130,10 +130,10 @@ void rasterize_triangle(driver_state& state, const data_geometry* in[3])
 
     for(unsigned int a = 0; a < 3; a++){
         int i = static_cast<int>((state.image_width / 2.0) 
-                * (*in)[a].gl_Position[0]/(*in)[a].gl_Position[3] 
+                * (*in)[a].gl_Position[0] 
                 + ((state.image_width / 2.0) - 0.5));
         int j = static_cast<int>((state.image_height / 2.0) 
-                * (*in)[a].gl_Position[1]/(*in)[a].gl_Position[3] 
+                * (*in)[a].gl_Position[1] 
                 + ((state.image_height / 2.0) - 0.5));
         x[a] = i;
         y[a] = j;
